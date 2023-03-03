d3c0e00d64eb78a4482eb7eb99e6aa3fc1a74bf2
Icasia-Rainstorm <luyijun0907@sjtu.edu.cn>
2023-03-03 05:48:48 +0000

    feat: environment setup

---
+++ env.sh
@@ -0,0 +1,7 @@
+
+echo -e "\033[45;37m---------install boost---------\033[0m"
+    sudo apt-get install libboost-all-dev
+
+echo -e "\033[45;37m---------install openssl---------\033[0m"
+    sudo apt-get install openssl
+    sudo apt-get install libssl-dev
